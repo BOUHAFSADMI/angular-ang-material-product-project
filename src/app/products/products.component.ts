19ddf62c84aa0b50208b7f7ceb3d9384a1061437
BOUHAFSADMI <sadmibouhafs@gmail.com>
2018-03-16 18:13:38 +0100

    added animations to list items

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  animations : [
+    trigger('productsAnim', [
+      state('active', style({
+        opacity: '1'
+      })),
+      transition('void => *', [
+        style({transform: 'translateX(-30px)', opacity: '0'}),
+        animate('700ms ease-in-out')
+      ]),
+      transition('* => void', [
+        animate('300ms ease-in-out', style({transform: 'translateX(-30px)', opacity: '0'}))
+      ])
+    ])
+  ]
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
 
   products = [
    {'title': 'Product1', 'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'},
@@ -15,10 +31,7 @@ export class ProductsComponent implements OnInit {
 
   title = '';
   description = '';
-
-  constructor() {  }
-
-  ngOnInit() {  }
+  state = 'active';
 
   addProduct(value: any) {
     this.products.push({'title': value.title, 'description': value.description});
